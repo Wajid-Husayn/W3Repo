c88e01d8feaf256270d6aceeae0526885454f642
Wajid Hussain <wajid.husayn@gmail.com>
2020-02-01 23:46:14 +0500

    added new program in remote repo

---
+++ src/Hellow.java
@@ -0,0 +1,13 @@
+
+public class Hellow {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int myNum = 15;
+		myNum = 20;  // myNum is now 20
+		System.out.println(myNum);
+	}
+
+	}
+
+
